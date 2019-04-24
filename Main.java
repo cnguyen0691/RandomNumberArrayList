7ef74afd9ee0044a230fd7731073aea964944e1d
cnguyen0691 <chaunguyen0691@gmail.com>
2019-04-24 15:37:32 -0400

    first commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+import java.util.Random;
+import java.util.ArrayList;
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numList = new ArrayList<Integer>();
+        Random ranNum = new Random();
+        int ranNumber;
+
+        //Initialize elements of an array
+        for (int i = 0; i < 8; i++) {
+            ranNumber = ranNum.nextInt(100);//creates a random number between 1 and 100
+            numList.add(ranNumber);
+        }System.out.println(numList);
+    }
+}
